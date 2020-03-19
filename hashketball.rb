7bf6e33bd4ce7c1c71557ac711862b4288872694
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-19 18:52:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,5 +134,15 @@ def num_points_scored(players_name)
 end
 
 def shoe_size(players_name)
-  
+  game_hash.each do |place, team|
+    team.each do |stats, data|
+      if stats == :players
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:shoe]
+          end
+        end
+      end
+    end
+  end
 end
