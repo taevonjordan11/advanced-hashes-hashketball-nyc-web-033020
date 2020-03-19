6d2ea69e2b352618b44c2fb8d6d394ff40777b3d
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-19 19:16:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -164,7 +164,8 @@ end
 def player_numbers(team_name)
   game_hash.map do |place, team|
     if team[:team_name] ==  team_name
-      binding.pry
+      team.each do 
+      end
     end
   end
 end
