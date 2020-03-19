09f69e0a03229246e3e7e6390a279e064e9ab87b
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-19 20:01:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,12 +196,12 @@ def player_stats(players_name)
 end
 
 def big_shoe_rebounds
-  big_shoes_guy = 0
+  count = 0
   rebounds = 0
     game_hash.each do | team, data|
       data[:players].each do |stats|
-        if stats[:shoe] > big_shoes_guy
-          big_shoes_guy = stats[:shoe]
+        if stats[:shoe] > count
+          count = stats[:shoe]
           rebounds = stats[:rebounds]
         end
       end
