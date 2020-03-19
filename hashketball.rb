79832b8a4b1bc0ffaecb3289d8328ab153fca57b
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-19 18:04:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,7 @@ def num_points_scored(players_name)
   game_hash.each do |place, team|
     team.each do |attribute, data|
       if attribute == :players
-        
+        data.each 
       end
     end
   end
